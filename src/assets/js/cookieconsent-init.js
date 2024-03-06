b0eeb7d6789847cbf6248f9bb5f18af06f4a9869
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-03-06 14:59:34 +0100

    Consent

--- src/assets/js/cookieconsent-init.js
+++ src/assets/js/cookieconsent-init.js
@@ -1,11 +1,12 @@
-window.addEventListener("load", function () {
+
+document.documentElement.classList.add('cc--darkmode');
+
 CookieConsent.run({
-    page_scripts: true,
     guiOptions: {
         consentModal: {
-            layout: "cloud",
-            position: "bottom center",
-            equalWeightButtons: false,
+            layout: "box",
+            position: "bottom right",
+            equalWeightButtons: true,
             flipButtons: false
         },
         preferencesModal: {
@@ -14,56 +15,67 @@ CookieConsent.run({
             equalWeightButtons: true,
             flipButtons: false
         }
-    },  
+    },
     categories: {
         necessary: {
-            enabled: true, 
-            readOnly: true  
+            readOnly: true
         },
-        analytics: {
-            enabled: true
-        }
+        functionality: {},
+        analytics: {},
+        marketing: {}
     },
     language: {
-        default: "pl",
+        default: "en",
         autoDetect: "browser",
         translations: {
-            pl: {
+            en: {
                 consentModal: {
-                    title: "Ta strona używa plików cookies",
-                    description: "Używam plików cookie do analizowania ruchu na stronie. Możesz wyłączyć skrypty śledzące w ustawieniach",
-                    acceptAllBtn: "Akceptuj",
-                    showPreferencesBtn: "Ustawienia"
+                    title: "Hello traveller, it's cookie time!",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
                 },
                 preferencesModal: {
-                    title: "Ustawienia plików cookies",
-                    acceptAllBtn: "Akceptuj",
-                    savePreferencesBtn: "Zapisz ustawienia",
-                    closeIconLabel: "Zamknij",
+                    title: "Consent Preferences Center",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    closeIconLabel: "Close modal",
                     serviceCounterLabel: "Service|Services",
                     sections: [
                         {
-                            title: "Wykorzystanie plików cookie",
-                            description: "Ponieważ szanuje Twoje prawo do prywatności, możesz nie zezwalać na niektóre rodzaje plików cookie. Kliknij nagłówki kategorii, żeby zmienić domyślne ustawienia."
+                            title: "Cookie Usage",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                         },
                         {
-                            title: "Niezbędne pliki cookie<span class=\"pm__badge\">Zawsze aktywne</span>",
-                            description: "Zazwyczaj są one ustawiane wyłącznie w odpowiedzi na podejmowane przez Ciebie działania, które są równoznaczne z żądaniem usług, jak np. ustawienie preferencji dotyczących prywatności, logowanie czy wypełnianie formularzy. Możesz ustawić swoją przeglądarkę tak, aby blokowała lub ostrzegała Cię o tych plikach cookie, ale niektóre części witryny nie będą wtedy działać. Te pliki cookie nie przechowują żadnych danych osobowych.",
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                             linkedCategory: "necessary"
                         },
                         {
-                            title: "Analityczne pliki cookie",
-                            description: "Te pliki cookie mogą być ustawiane za pośrednictwem naszej witryny przez naszych partnerów reklamowych. Mogą być one wykorzystywane przez te firmy do tworzenia profilu Twoich zainteresowań i wyświetlania odpowiednich reklam w innych witrynach. Nie przechowują bezpośrednio danych osobowych, ale opierają się na jednoznacznej identyfikacji Twojej przeglądarki i urządzenia internetowego. ",
+                            title: "Functionality Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                             linkedCategory: "analytics"
                         },
                         {
-                            title: "Więcej informacji",
-                            description: "W przypadku jakichkolwiek pytań związanych z polityką dotyczącą plików cookie, proszę <a class=\"cc__link\" href=\"mailto:kontakt@futurewebstudio.pl\">skontaktuj się</a>."
+                            title: "Advertisement Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "marketing"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
                         }
                     ]
                 }
             }
         }
     }
-});
 });
