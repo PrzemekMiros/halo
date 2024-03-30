005b5c5d96af5bc0367435fc15129d0f82d8b744
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-03-30 22:25:43 +0100

    Remove  gradient

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -186,100 +186,83 @@ if (document.querySelector('.swiper-works')) {
 
 
 
-if(document.querySelector('.multiform')) {
+if(document.querySelector('.form-outer')) {
+  initMultiStepForm();
+
+  function initMultiStepForm() {
+      const progressNumber = document.querySelectorAll(".step").length;
+      const slidePage = document.querySelector(".slide-page");
+      const submitBtn = document.querySelector(".submit");
+      const progressCheck = document.querySelectorAll(".step .check");
+      const bullet = document.querySelectorAll(".step .bullet");
+      const pages = document.querySelectorAll(".page");
+      const nextButtons = document.querySelectorAll(".next");
+      const prevButtons = document.querySelectorAll(".prev");
+      const stepsNumber = pages.length;
   
-const previousButton = document.querySelector('#prev')
-const nextButton = document.querySelector('#next')
-const submitButton = document.querySelector('#submit')
-const tabTargets = document.querySelectorAll('.tab')
-const tabPanels = document.querySelectorAll('.tabpanel')
-const isEmpty = (str) => !str.trim().length
-let currentStep = 0
-
-// Validate first input on load
-validateEntry()
-
-// Next: Change UI relative to current step and account for button permissions
-nextButton.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  // Hide current tab
-  tabPanels[currentStep].classList.add('hidden')
-  tabTargets[currentStep].classList.remove('active')
-
-  // Show next tab
-  tabPanels[currentStep + 1].classList.remove('hidden')
-  tabTargets[currentStep + 1].classList.add('active')
-  currentStep += 1
+      if (progressNumber !== stepsNumber) {
+          console.warn(
+              "Error, number of steps in progress bar do not match number of pages"
+          );
+      }
   
-  validateEntry()
-  updateStatusDisplay()
-})
+      document.documentElement.style.setProperty("--stepNumber", stepsNumber);
+  
+      let current = 1;
+  
+      for (let i = 0; i < nextButtons.length; i++) {
+          nextButtons[i].addEventListener("click", function (event) {
+              event.preventDefault();
+  
+              inputsValid = validateInputs(this);
+              // inputsValid = true;
+  
+              if (inputsValid) {
+                  slidePage.style.marginLeft = `-${
+                      (100 / stepsNumber) * current
+                  }%`;
+                  bullet[current - 1].classList.add("active");
+                  progressCheck[current - 1].classList.add("active");
+                  current += 1;
+              }
+          });
+      }
+  
+      for (let i = 0; i < prevButtons.length; i++) {
+          prevButtons[i].addEventListener("click", function (event) {
+              event.preventDefault();
+              slidePage.style.marginLeft = `-${
+                  (100 / stepsNumber) * (current - 2)
+              }%`;
+              bullet[current - 2].classList.remove("active");
+              progressCheck[current - 2].classList.remove("active");
+              current -= 1;
+          });
+      }
+      submitBtn.addEventListener("click", function () {
+        
+          inputsValid = validateInputs(this);
 
-// Previous: Change UI relative to current step and account for button permissions
-previousButton.addEventListener('click', (event) => {
-  event.preventDefault()
-
-  // Hide current tab
-  tabPanels[currentStep].classList.add('hidden')
-  tabTargets[currentStep].classList.remove('active')
-
-  // Show previous tab
-  tabPanels[currentStep - 1].classList.remove('hidden')
-  tabTargets[currentStep - 1].classList.add('active')
-  currentStep -= 1
-
-  nextButton.removeAttribute('disabled')
-  updateStatusDisplay()
-})
-
-
-function updateStatusDisplay() {
-  // If on the last step, hide the next button and show submit
-  if (currentStep === tabTargets.length - 1) {
-    nextButton.classList.add('hidden')
-    previousButton.classList.remove('hidden')
-    submitButton.classList.remove('hidden')
-    validateEntry()
-
-    // If it's the first step hide the previous button
-  } else if (currentStep == 0) {
-    nextButton.classList.remove('hidden')
-    previousButton.classList.add('hidden')
-    submitButton.classList.add('hidden')
-    // In all other instances display both buttons
-  } else {
-    nextButton.classList.remove('hidden')
-    previousButton.classList.remove('hidden')
-    submitButton.classList.add('hidden')
+      });
+  
+      function validateInputs(ths) {
+          let inputsValid = true;
+  
+          const inputs =
+              ths.parentElement.parentElement.querySelectorAll("input");
+          for (let i = 0; i < inputs.length; i++) {
+              const valid = inputs[i].checkValidity();
+              if (!valid) {
+                  inputsValid = false;
+                  inputs[i].classList.add("invalid-input");
+              } else {
+                  inputs[i].classList.remove("invalid-input");
+              }
+          }
+          return inputsValid;
+      }
   }
-}
-
-function validateEntry() {
-  let input = tabPanels[currentStep].querySelector('.form-input')
   
-  // Start but disabling continue button
-  nextButton.setAttribute('disabled', true)
-  submitButton.setAttribute('disabled', true)
-  
-  // Validate on initial function fire
-  setButtonPermissions(input)
-  
-  // Validate on input
-  input.addEventListener('input', () => setButtonPermissions(input))
-  // Validate if bluring from input
-  input.addEventListener('blur', () => setButtonPermissions(input))
-}
-
-function setButtonPermissions(input) {
-  if (isEmpty(input.value) && input.classList.contains('required') ) { 
-    nextButton.setAttribute('disabled', true)
-    submitButton.setAttribute('disabled', true)
-  } else {
-    nextButton.removeAttribute('disabled')
-    submitButton.removeAttribute('disabled')
-  }
-}
 }
 
 
