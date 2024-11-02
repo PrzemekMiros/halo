83d9e0c561035a5a35d67acc83e4d0430dedbb76
PrzemekMiros <kontakt@futurewebstudio.pl>
2024-11-02 15:47:18 +0100

    Remove AJAX

--- .eleventy.js
+++ .eleventy.js
@@ -90,7 +90,7 @@ module.exports = function(eleventyConfig) {
             height="${largestSrc.height}">`;
  
           return `<div class="image-wrapper blur-load" >
-                    <img class="placeholder" src="${lowestSrc.url}" loading="lazy" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
+                    <img class="placeholder" src="${lowestSrc.url}" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
         });
 
         eleventyConfig.addNunjucksAsyncShortcode('workImage', async (src, alt) => {
@@ -131,7 +131,7 @@ module.exports = function(eleventyConfig) {
             height="${lowestSrc.height}">`;
       
             return `<div class="image-wrapper blur-load" >
-              <img class="placeholder" src="${lowestSrc.url}" loading="lazy" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
+              <img class="placeholder" src="${lowestSrc.url}" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
         });
         
         eleventyConfig.addNunjucksAsyncShortcode('blogImage', async (src, alt) => {
@@ -172,7 +172,7 @@ module.exports = function(eleventyConfig) {
             height="${lowestSrc.height}">`;
       
             return `<div class="image-wrapper blur-load" >
-              <img class="placeholder" src="${lowestSrc.url}" loading="lazy" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
+              <img class="placeholder" src="${lowestSrc.url}" alt="Placeholder" width="${largestSrc.width}" height="${largestSrc.height}"><picture> ${source} ${img} </picture></div>`;
         });
 
       // Code blocks
